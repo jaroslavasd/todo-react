da0bdc7a188b607de4a6a278f9be1e7d462513ba
jaroslavasd <jaroslavasd@wix.com>
2023-02-14 13:49:41 +0200

    add qr to footer

--- src/comp/Footer.jsx
+++ src/comp/Footer.jsx
@@ -1,3 +1,4 @@
+import qrcode from '../img/qrcode.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,8 +7,19 @@ export function Footer() {
   return (
     <div className="footer">
       <Container fluid className="bg-white">
-        <Row>
-          <Col>@Wix by Simona and Jaroslavas</Col>
+        <Row className="justify-content-md-center">
+          <Col>by Simona and Jaroslavas @Wix</Col>
+        </Row>
+        <Row className="md-center">
+          <Col>
+            <img
+              alt=""
+              src={qrcode}
+              width="150em"
+              height="150em"
+              className="d-inline-block align-center"
+            />
+          </Col>
         </Row>
       </Container>
     </div>
