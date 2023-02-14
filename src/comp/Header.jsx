91d6974cd93d1feeaa48160094166ac39017d2a0
jaroslavasd <jaroslavasd@wix.com>
2023-02-14 12:53:12 +0200

    add header size

--- src/comp/Header.jsx
+++ src/comp/Header.jsx
@@ -7,13 +7,13 @@ export function Header() {
     <div className="header">
       <Navbar bg="light" variant="light" fixed="top">
         <Container fluid style={{ width: '70%' }}>
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="#home" style={{ fontSize: '18px' }}>
             <img
               alt=""
               src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
+              width="30px"
+              height="30px"
+              className="d-inline-block align-center"
             />{' '}
             TODO Awesome React App
           </Navbar.Brand>
